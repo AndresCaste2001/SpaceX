4de3ed6fb623eb635eefbb7642ddb79a635caeca
AndresCaste2001 <143757955+AndresCaste2001@users.noreply.github.com>
2024-05-03 20:49:21 +0000

    feat: :sparkles: Js Modules
    se actualizaron los modulos a la version actual del profesor

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import { 
+    paginationRockets
+} from "./modulesComponents/pagination.js";
+
+let paginacion = document.querySelector("#paginacion");
+paginacion.append(await paginationRockets())
